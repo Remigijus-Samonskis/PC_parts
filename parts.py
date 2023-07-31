3536f3bf4c37d7e7113ef67ee2ff15f21e2ffe4a
name surname <full@email.com>
2023-07-31 16:23:36 +0300

    parts.py

---
+++ parts.py
@@ -0,0 +1,26 @@
+PARTS_INFO = {
+    1: {
+        "name": "Intel HD Graphics 4600",
+        "core_count": 4,
+        "performance_core_clock": "3.5 GHz",
+        "performance_boost_clock": "3.9 GHz",
+        "TDP": "50 W",
+        "price": "$190",
+    },
+    2: {
+        "name": "Intel UHD Graphics 630",
+        "core_count": 4,
+        "performance_core_clock": "3.6 GHz",
+        "performance_boost_clock": "4.3 GHz",
+        "TDP": "65 W",
+        "price": "$105",
+    },
+    3: {
+        "name": "Intel UHD Graphics 874",
+        "core_count": 32,
+        "performance_core_clock": "3.7 GHz",
+        "performance_boost_clock": "4.5 GHz",
+        "TDP": "280 W",
+        "price": "$2300",
+    },
+}
